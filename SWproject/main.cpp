a1de8ad114c47e0ba5369b896344d6ef2ad37043
core-not-dumped <dltmd150722@gmail.com>
2021-11-19 19:58:01 +0900

    Skeleton_1 - Seung Tae

--- SWproject/main.cpp
+++ SWproject/main.cpp
@@ -1,26 +1,69 @@
-#include <stdio.h>
-#include <windows.h>
-#include <stdlib.h>
-#include <iostream>
+#include<iostream>
+#include<Windows.h>
+#include<conio.h>
+#include<vector>
+#include<string>
+#include<ctime>
 
 using namespace std;
 
 void gotoxy(int x, int y);
+char start_game(void);
+void show_manual(void);
 
-int main() {
-	printf("1221");
-	Sleep(2000);
-	printf("122122");
-	while (1) {
-		system("cls");
-		gotoxy(5, 2);
-		cout << "***********fwqifwq********";
-		Sleep(2000);
+enum command
+{
+	GAMESTART = '1',
+	MANUAL,
+	QUIT
+};
+
+int main()
+{
+	while (1)
+	{
+		switch (start_game())
+		{
+		case GAMESTART:
+			cout << "1\n";
+			break;
+		case MANUAL:
+			show_manual();
+			break;
+		case QUIT:
+			return 0;
+		}
 	}
-	return 0;
 }
 
+char start_game(void)
+{
+	system("cls");
+	cout << "Welcome!!\n";
+	cout << "Press 1 to start game!!\n";
+	cout << "Press 2 to see manual!!\n";
+	cout << "Press 3 to exit the game!!\n";
 
+	char c;
+	cin >> c;
+	return c;
+}
+
+void show_manual(void)
+{
+	while (1)
+	{
+		system("cls");
+		cout << "abcd\n";
+		cout << "Press 1 button to return the start screen\n";
+
+		char c = '0';
+		cin >> c;
+		if (c == '1')	break;
+		else			continue;
+	}
+	return;
+}
 
 void gotoxy(int x, int y)
 {
